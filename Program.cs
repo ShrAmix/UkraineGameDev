e5ec9b73dccb329261cc067e57bf16944e9b2dc5
ShrAmix <ifoxps@gmail.com>
2024-09-17 23:11:27 +0300

    FinalSite

--- Program.cs
+++ Program.cs
@@ -1,13 +1,3 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using UkraineGameDev.Data;
-using UkraineGameDev.Models;
-
 namespace UkraineGameDev
 {
     public class Program
@@ -16,40 +6,22 @@ namespace UkraineGameDev
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Configure DbContext
-            builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-
-            // Configure Identity
-            builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
-            {
-                // Password settings
-                options.Password.RequireDigit = false;
-                options.Password.RequireLowercase = false;
-                options.Password.RequireUppercase = false;
-                options.Password.RequireNonAlphanumeric = false;
-                options.Password.RequiredLength = 8;
-                options.Password.RequiredUniqueChars = 0;
-            })
-.AddEntityFrameworkStores<ApplicationDbContext>()
-.AddDefaultTokenProviders();
-
-            // Add services to the container
+            // Add services to the container.
             builder.Services.AddRazorPages();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline
+            // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Error");
                 app.UseHsts();
             }
+            app.UseStaticFiles();
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseRouting();
-            app.UseAuthentication(); // Ensure authentication middleware is added
             app.UseAuthorization();
             app.MapRazorPages();
             app.Run();
